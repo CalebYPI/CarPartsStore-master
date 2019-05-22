fbd6a2de63a19fe437038fbc09186f0eef9084d6
Caleb Ruiters <35923446+CalebYPI@users.noreply.github.com>
2019-05-22 09:47:19 +0200

    Major Test Updates 2.0

--- CarPartsStore/src/test/java/za/ac/cput/group3b/factory/Staff/StaffFactoryTest.java
+++ CarPartsStore/src/test/java/za/ac/cput/group3b/factory/Staff/StaffFactoryTest.java
@@ -1,21 +1,4 @@
 package za.ac.cput.group3b.factory.Staff;
 
-import org.junit.Assert;
-import org.junit.Test;
-import za.ac.cput.group3b.domain.Staff.Staff;
-
-import static org.junit.Assert.*;
-
 public class StaffFactoryTest {
-
-    @Test
-    public void getStaff() {
-        int staffId = 101;
-        String staffFName = "Bob";
-        String staffLName = "The Builder";
-        String staffJobTitle = "Main Mechanic";
-        Staff emp1 = StaffFactory.getStaff(staffId, staffFName, staffLName, staffJobTitle);
-        System.out.println(emp1);
-        Assert.assertNotNull(emp1.getStaffId());
-    }
 }
